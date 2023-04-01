baf070321c259d7b3dd23321f2346a1fe3a05856
BeyonderSS <ssbeyonder@gmail.com>
2023-04-01 15:48:11 +0530

    mmain

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Header from '@/components/Header'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-  <>
-  <h1 className='bg-yellow-300'>hello</h1>
-  </>
+  <div>
+  <Header/>
+  </div>
   )
 }
