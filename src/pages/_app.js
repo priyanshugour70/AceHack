52fd187516e0c3998125593dfdb447993d25e1ec
Priyanshu Gour <priyanshugournotes1@gmail.com>
2023-04-02 10:30:26 +0530

    main

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
 
@@ -6,7 +7,7 @@ export default function App({ Component, pageProps }) {
     <div>
       <Header/>
       <Component {...pageProps} />{" "}
-      
+      <Footer />
     </div>
 
   );
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,17 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Header from '@/components/Header'
-import AppointmentsToday from './doc/AppointmentsToday'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { Inter } from "next/font/google";
+import Header from "@/components/Header";
+import AppointmentsToday from "./doc/AppointmentsToday";
 // import AppointmentRequest from '@/components/Appointmentsrequest'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return (
-  <div>
-    
-  
- </div>
-   
-  )
+    return (
+        <div>
+           Home
+        </div>
+    );
 }
